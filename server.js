a13e8ae20f3d04308d5828426041236fce6aaee8
Estefania Rodriguez <105613482+EstefaniaJRodriguez@users.noreply.github.com>
2025-11-25 23:27:12 -0300

    .

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'https://accountant-application-front.onrender.com',
-  'https://www.genimpositivo.com/' // 🔹 reemplazalo con tu URL de frontend
+  'https://www.genimpositivo.com/',
+  'https://www.genimpositivo.com' // 🔹 reemplazalo con tu URL de frontend
 ];
 
 const corsOptions = {
