a4e83210b2237b4aedae57fc31bcb27e8ff5b962
Estefania Rodriguez <105613482+EstefaniaJRodriguez@users.noreply.github.com>
2025-12-05 23:27:20 +1300

    .

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,18 +1,20 @@
 import jwt from "jsonwebtoken";
 
-export function authMiddleware(req, res, next) {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader)
-    return res.status(401).json({ message: "Token no enviado" });
-
-  const token = authHeader.split(" ")[1];
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err)
-      return res.status(403).json({ message: "Token inválido o expirado" });
-
-    req.user = user;
-    next();
-  });
-}
+export default function verifyToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+  
+    if (!token) {
+      return res.status(401).json({ error: "Token requerido" });
+    }
+  
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.status(403).json({ error: "Token inválido" });
+      }
+  
+      req.user = user;
+      next();
+    });
+  }
+  
